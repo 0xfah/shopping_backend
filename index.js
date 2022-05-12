7f5a576f22c8474de94dd62c0387b32272eadc18
fam <test@example.com>
2022-05-13 04:22:24 +0800

    save men options
    retrive mens options

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'myProject';
+const dbName = 'shopping';
+
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('documents');
-
+  const collection = db.collection('men');
+  //  await collection.insertMany([{name: "man1", price: 20},{ name: "man2", price: 30}, {name: "man3", price: 40}, {name: "man4", price: 50}])
 
   return 'done.';
 }
@@ -28,11 +29,12 @@ async function main() {
 main()
   .then(console.log)
   .catch(console.error)
-  .finally(() => client.close());
 
 
-app.get('/men-names', (req, res) => {
-    res.json('test')
+app.get('/men', async (req, res) => {
+  const db = client.db(dbName);
+  const results = await db.collection('men').find({}).toArray();
+  return res.jsonp(results)
 })
 
 
